2b4c57a97abeb92b06fe4d92bb9cb1499afa8866
Miroslav Todorov <smash.all22@outlook.com>
2024-11-04 18:41:15 +0200

    #Additional classes

---
+++ src/test/java/Helpers/TextFieldsLocators.java
@@ -0,0 +1,7 @@
+package Helpers;
+
+public class TextFieldsLocators {
+    public static final String FIRST_NAME_FIELD = "forename";
+    public static final String LAST_NAME_FIELD = "//input[@name='map(lastName)']";
+    public static final String EMAIL_FIELD = "input[name='map(email)']";
+}
---
+++ src/test/java/Helpers/TextInputs.java
@@ -0,0 +1,7 @@
+package Helpers;
+
+public class TextInputs {
+    public static final String USER_FIRST_NAME_FIELD = "UserLastName";
+    public static final String USER_LAST_NAME_FIELD = "UserLastName";
+    public static final String USER_MAIL = "test_user_mail@gmail.com";
+}
