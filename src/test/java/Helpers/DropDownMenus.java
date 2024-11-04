81809f90c1e3d851803313a06a27bba57b70a0b1
Miroslav Todorov <smash.all22@outlook.com>
2024-11-04 18:38:16 +0200

    #Added another class with locators

---
+++ src/test/java/Helpers/DropDownMenus.java
@@ -0,0 +1,12 @@
+package Helpers;
+
+public class DropDownMenus {
+    public static final String ABOUT_YOU_TITLE = "title";
+    public static final String ABOUT_YOU_TITLE_VALUE = "//*[@id=\"title\"]/option[2]";
+    public static final String DATE_DAY_OF_BIRTH = "dobDay";
+    public static final String DATE_DAY = "//*[@id='dobDay']/option[16]";
+    public static final String DATE_MONTH_OF_BIRTH = "dobMonth";
+    public static final String DATE_MONTH = "//*[@id='dobMonth']/option[12]";
+    public static final String DATE_YEAR_OF_BIRTH = "dobYear";
+    public static final String DATE_YEAR = "//*[@id='dobYear']/option[16]";
+}
